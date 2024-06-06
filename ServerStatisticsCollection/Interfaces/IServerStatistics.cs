305a812ce774f0631b26493b340953483ff696d0
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-06-06 21:32:42 +0300

    Add ServerStatistics Model and Interface.

---
+++ ServerStatisticsCollection/Interfaces/IServerStatistics.cs
@@ -0,0 +1,9 @@
+﻿namespace ServerStatisticsCollection.Interfaces;
+
+public interface IServerStatistics
+{
+    public double MemoryUsage { get; set; }
+    public double AvailableMemory { get; set; }
+    public double CpuUsage { get; set; }
+    public DateTime Timestamp { get; set; }
+}
---
+++ ServerStatisticsCollection/Models/ServerStatistics.cs
@@ -0,0 +1,25 @@
+﻿using ServerStatisticsCollection.Interfaces;
+
+namespace ServerStatisticsCollection.Models;
+
+public class ServerStatistics : IServerStatistics
+{
+    public double MemoryUsage { get; set; }
+    public double AvailableMemory { get; set; }
+    public double CpuUsage { get; set; }
+    public DateTime Timestamp { get; set; }
+
+    public ServerStatistics(double memoryUsage, double availableMemory, double cpuUsage, DateTime timestamp)
+    {
+        MemoryUsage = memoryUsage;
+        AvailableMemory = availableMemory;
+        CpuUsage = cpuUsage;
+        Timestamp = timestamp;
+    }
+
+    public override string ToString()
+    {
+        return
+            $"Memory Usage: {MemoryUsage} MB, Available Memory: {AvailableMemory} MB, CPU Usage: {CpuUsage}%, Timestamp: {Timestamp}";
+    }
+}
