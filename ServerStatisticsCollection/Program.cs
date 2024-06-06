296a5f7a582b6552f17e0fd6b58b3ac276cb3c3e
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-06-06 21:51:38 +0300

    Implement Program.cs that tests message publishing.

--- ServerStatisticsCollection/Program.cs
+++ ServerStatisticsCollection/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using RabbitMQ.Client;
+using ServerStatisticsCollection.Configurations;
+using ServerStatisticsCollection.Interfaces;
+using ServerStatisticsCollection.Messaging;
+using ServerStatisticsCollection.Services;
 
-Console.WriteLine("Hello, World!");
+namespace ServerStatisticsCollection;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var currentNamespace = typeof(Program).Namespace?.Split('.')[0];
+
+        var configManager = new ConfigurationManager(currentNamespace);
+
+        if (int.TryParse(configManager.GetData("ServerStatisticsConfig", "SamplingIntervalSeconds"),
+                out var samplingIntervalSeconds))
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            IMessageQueuePublisher messageQueuePublisher = new RabbitMqMessageQueuePublisher(factory);
+
+            var collector = new StatisticsCollector(samplingIntervalSeconds, messageQueuePublisher, configManager);
+
+            using (var cancellationTokenSource = new CancellationTokenSource())
+            {
+                Console.CancelKeyPress += (sender, eventArgs) =>
+                {
+                    eventArgs.Cancel = true;
+                    cancellationTokenSource.Cancel();
+                };
+
+                await collector.StartCollectingAsync(cancellationTokenSource.Token, "ServerStatistics");
+            }
+        }
+    }
+}
