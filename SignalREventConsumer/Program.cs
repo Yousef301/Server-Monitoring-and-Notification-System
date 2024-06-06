106ba6b78fc598f2791670e5680226756f3d9ef2
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-06-06 22:46:55 +0300

    Implement SignalR event consumer service.

---
+++ SignalREventConsumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using ServerStatisticsCollection.Configurations;
+
+namespace SignalREventConsumer;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var currentNamespace = typeof(Program).Namespace?.Split('.')[0];
+
+        var configManager = new ConfigurationManager(currentNamespace);
+        var signalRUrl = configManager.GetSection("SignalRConfig")["SignalRUrl"];
+
+        if (signalRUrl == null)
+        {
+            return;
+        }
+
+        var connection = new HubConnectionBuilder()
+            .WithUrl("http://localhost:5000/AlertHub")
+            .Build();
+
+        await connection.StartAsync();
+
+        connection.On("ReceiveMessage",
+            (string user, string message) => { Console.WriteLine($"{user}: {message}"); });
+
+        Console.ReadKey();
+    }
+}
