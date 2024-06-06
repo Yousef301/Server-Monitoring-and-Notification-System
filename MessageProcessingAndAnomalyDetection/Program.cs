4922dd8c35ef6642b51ca9fe36513b4888e81192
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-06-06 22:08:29 +0300

    Test if everything works correctly with mongodb

--- MessageProcessingAndAnomalyDetection/Program.cs
+++ MessageProcessingAndAnomalyDetection/Program.cs
@@ -1,5 +1,7 @@
 ﻿using MessageProcessingAndAnomalyDetection.Messaging;
 using MessageProcessingAndAnomalyDetection.Models;
+using MessageProcessingAndAnomalyDetection.Repositories;
+using MongoDB.Driver;
 using RabbitMQ.Client;
 using ServerStatisticsCollection.Configurations;
 
@@ -9,8 +11,28 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var factory = new ConnectionFactory() { HostName = "localhost" };
-        var messageQueue = new RabbitMqMessageQueueConsumer(factory);
-        messageQueue.StartConsuming<ServerStatistics>("ServerStatistics", "ServerStatistics");
+        var currentNamespace = typeof(Program).Namespace?.Split('.')[0];
+
+        var configManager = new ConfigurationManager(currentNamespace);
+
+        var connectionString = configManager.GetSection("MongoDBConnectionStrings");
+
+
+        if (connectionString != null)
+        {
+            var mongoDb = GetMongoDb(connectionString["ConnectionString"], connectionString["DatabaseName"]);
+            var serverStatisticsMongoDbRepository = new ServerStatisticsMongoDbRepository(mongoDb);
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var messageQueue =
+                new RabbitMqMessageQueueConsumer(factory, serverStatisticsMongoDbRepository);
+            messageQueue.StartConsuming<ServerStatistics>("ServerStatistics", "ServerStatistics");
+        }
+    }
+
+    private static IMongoDatabase GetMongoDb(string? connectionString, string? databaseName)
+    {
+        var client = new MongoClient(connectionString);
+        return client.GetDatabase(databaseName);
     }
 }
